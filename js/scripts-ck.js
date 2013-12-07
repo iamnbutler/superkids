6f16aee3640a6b23cdfcd58c4863701f16e2277a
Nathan Butler <iamnbutler@gmail.com>
2013-12-06 23:34:02 -0500

    big updates yo

--- js/scripts-ck.js
+++ js/scripts-ck.js
@@ -1,2 +1,2 @@
 // Load scripts after page loads
-$(document).ready(function(){$(".toc-target").click(function(){$(this).parent().toggleClass("open");return!1})});
+$(document).ready(function(){$.localScroll();$(".toc a").click(function(){$(".toc").removeClass("open");return!1});$(".toc-target").click(function(){$(this).parent().toggleClass("open");return!1});$("#panel-4-jobs").waypoint(function(e){$(".jobs-infographic").addClass("active")},{offset:"60%"});$("#panel-4-salary").waypoint(function(e){$(".salary-infographic").addClass("active")},{offset:"60%"});$("#panel-4-gender").waypoint(function(e){$(".gender-infographic").addClass("active")},{offset:"60%"});$("#theprocess").waypoint(function(e){$(".steps").addClass("active")},{offset:"20%"})});
--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,27 @@
 // Load scripts after page loads
 $(document).ready(function(){
+	// Localscroll the nav
+	$.localScroll();
+
+	$('.toc a').click(function(){
+		$('.toc').removeClass('open');
+		return false;
+	});
+
 	$('.toc-target').click(function(){
 		$(this).parent().toggleClass('open');
 		return false;
 	});
+	$('#panel-4-jobs').waypoint(function(down) {
+	  $('.jobs-infographic').addClass('active');
+	}, { offset: '60%' });
+	$('#panel-4-salary').waypoint(function(down) {
+	  $('.salary-infographic').addClass('active');
+	}, { offset: '60%' });
+	$('#panel-4-gender').waypoint(function(down) {
+	  $('.gender-infographic').addClass('active');
+	}, { offset: '60%' });
+	$('#theprocess').waypoint(function(down) {
+	  $('.steps').addClass('active');
+	}, { offset: '20%' });
 });
